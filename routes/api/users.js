2fd6a774a9e6ca55f3ed9f072bdaeadba3f684bf
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2019-02-12 22:10:23 -0500

    Adding files which is responsible for routing

---
+++ routes/api/users.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const router = express.Router();
+const key = require('../../config/keys');
+const registerValidation = require('../../register');
+const loginValidation = require('../../login');
+const User = require('../../models/User')
+
+
+router.post('/register', (req,res) =>{
+  console.log(req.body);
+  const {errors , isValid} = registerValidation(req.body);
+  if(!isValid){
+    res.status(400).json(errors);
+  }else{
+    User.findOne({email:req.body.email})
+    .then(user =>{
+      if(user){
+        res.status(400).json({email:'Email Already Exists'});
+      }else{
+        const newUser = new User({
+          email : req.body.email,
+          name : req.body.name,
+          password : req.body.password
+        });
+
+        newUser.save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
+
+      }
+    });
+  }
+
+});
+
+
+router.post('/login', (req,res)=>{
+  const {erros, isValid} = loginValidation(req.body);
+  if(!isValid){
+    res.status(400).json(errors);
+  }else{
+    User.findOne({email : req.body.email})
+    .then(user =>{
+      if(!user){
+        res.status(400).json({email : 'Invalid Email Address'});
+      }
+      if(user.password.localeCompare(req.body.password)!=0){
+        res.status(400).json({password : 'Password is incorrect'});
+      }
+      res.json({
+        success: true,
+        email : 'Logged In'
+      });
+    })
+  }
+});
+
+module.exports = router;
