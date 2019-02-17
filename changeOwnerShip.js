b2276ff7758b8adffa8efda713b1d8d01b56c294
Vishnu Varshath Harishankar <varshath@Vishnus-MBP.fios-router.home>
2019-02-16 20:56:07 -0500

    Adding Transaction for settlement

--- changeOwnerShip.js
+++ changeOwnerShip.js
@@ -1,39 +1,53 @@
 const User = require('./models/User')
 const express = require('express');
 const router = express.Router();
+const Transaction = require('mongoose-transactions')
+const transaction = new Transaction()
 
+async function settle(user,old_user,cost,callback){
 
+  var newOwnerId = user._id;
+  var oldOwnerId = old_user._id;
+  console.log(old_user.accountBalance+cost);
+
+  const deduct = {
+    accountBalance : user.accountBalance - Number.parseInt(cost)
+  }
+  const credit = {
+    accountBalance : old_user.accountBalance + Number.parseInt(cost)
+  }
+  try {
+    transaction.update("users", newOwnerId, deduct);
+    transaction.update("users", oldOwnerId, credit);
+    const final = await transaction.run()
+} catch (error) {
+    console.error(error)
+    const rollbackObj = await transaction.rollback().catch(console.error)
+    transaction.clean()
+    callback(false);
+  }
+}
 
 
 module.exports = function changeOwnerShip(data,callback){
-  var prev_owner = data.prev_owner;
+  var prev_owner = data.old_owner;
   var new_owner = data.new_owner;
   var cost = data.cost;
   this.isValid = false;
   User.findOne({email : new_owner})
   .then(user =>{
     if(user.accountBalance<Number.parseInt(cost)){
-      //console.log(user.accountBalance,Number.parseInt(cost));
-      this.isValid=false;
-      callback(this.isValid);
+      //console.log(user.accountBalance,Number.parseInt(cost))
+      callback(false);
     }else{
-      const newOwner = new User({
-        _id : user.id,
-        email : user.email,
-        name : user.name,
-        password : user.password,
-        accountBalance : user.accountBalance-cost
+      console.log(prev_owner);
+      User.findOne({email : prev_owner})
+      .then(old_user => {
+          //console.log(prev_owner);
+          settle(user,old_user,cost,callback);
+          console.log("Transaction completed");
+          callback(true);
       });
-      var query = { _id : user.id };
-      User.findOneAndUpdate(query, { $set: { accountBalance : user.accountBalance-cost }}, function(err,user){
-        if(err){
-          console.log("Could not deduct");
-        }else{
-          console.log("Successfully deducted");
-        }
-      });
-      this.isValid=true;
-      callback(this.isValid);
     }
   });
 };
